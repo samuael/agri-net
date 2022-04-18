333ca1455a393100e80cf027b4ff80e822264f0b
belayhun-arage <belayhun24arage@gmail.com>
2022-04-18 02:34:04 -0400

    product add and delete

--- agri-net-backend/entity/productEntity.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type Product struct {
-	Id       int     `json:"id"`
-	Name     string  `json:"name"`
-	Location string  `json:"location"`
-	Amounte  string  `json:"amounte"`
-	Price    float64 `json:"price"`
-}
--- agri-net-backend/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/belayhun-arage/AgriNetBack
-
-go 1.13
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/lib/pq v1.10.5
-)

05bf42af1422c2433300a5a4a8ba2b7d6e8fb2c3
DavG20 <dawit201121@gmail.com>
2022-06-18 21:30:27 +0300

    pro page

---
+++ agri-info-frontend/mobile/lib/Products/screen/product_contract.dart
@@ -0,0 +1,17 @@
+import 'package:mobile/libs.dart';
+
+class ProductContract extends StatefulWidget {
+  const ProductContract({ Key? key }) : super(key: key);
+
+  @override
+  State<ProductContract> createState() => _ProductContractState();
+}
+
+class _ProductContractState extends State<ProductContract> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
